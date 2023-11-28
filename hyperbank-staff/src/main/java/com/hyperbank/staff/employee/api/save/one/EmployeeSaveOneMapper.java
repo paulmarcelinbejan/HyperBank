5c6f20a1d2625caa3c33db011f18177ee92f5914
Paul Marcelin Bejan <paulmarcelinbejan.developer@gmail.com>
2023-11-28 17:55:54 +0100

    feat: employee - saveOne

--- hyperbank-staff/src/main/java/com/hyperbank/staff/employee/api/save/one/EmployeeSaveOneMapper.java
+++ hyperbank-staff/src/main/java/com/hyperbank/staff/employee/api/save/one/EmployeeSaveOneMapper.java
@@ -1,28 +1,28 @@
 package com.hyperbank.staff.employee.api.save.one;
 
+import org.mapstruct.InheritConfiguration;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.MappingConstants;
 import org.mapstruct.Named;
 
 import com.hyperbank.staff.employee.dto.EmployeeResponse;
 import com.hyperbank.staff.employee.dto.EmployeeSaveRequest;
 import com.hyperbank.staff.employee.entity.Employee;
+import com.hyperbank.staff.employee.mapper.EmployeeMapperConfig;
 import com.paulmarcelinbejan.architecture.sniper.mapper.input.MapperInput;
 import com.paulmarcelinbejan.architecture.sniper.mapper.output.MapperOutput;
 
-@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
-public abstract class EmployeeSaveOneMapper implements 
+@Mapper(config = EmployeeMapperConfig.class)
+public interface EmployeeSaveOneMapper extends 
 	MapperInput<EmployeeSaveRequest, Employee>,
 	MapperOutput<Employee, EmployeeResponse> {
 	
 	@Override
-	@Named("toDomain")
-	@Mapping(target = "id", ignore = true)
-	public abstract Employee toDomain(EmployeeSaveRequest request);
+	@Named("fromRequestToDomain")
+	@InheritConfiguration
+	Employee toDomain(EmployeeSaveRequest request);
 	
 	@Override
-	@Named("toResponse")
-	public abstract EmployeeResponse toResponse(Employee entity);
+	@InheritConfiguration
+	EmployeeResponse toResponse(Employee entity);
 	
 }
--- hyperbank-staff/src/main/java/com/hyperbank/staff/employee/api/save/one/EmployeeSaveOneSniper.java
+++ hyperbank-staff/src/main/java/com/hyperbank/staff/employee/api/save/one/EmployeeSaveOneSniper.java
@@ -4,21 +4,16 @@ import com.hyperbank.staff.employee.dto.EmployeeResponse;
 import com.hyperbank.staff.employee.dto.EmployeeSaveRequest;
 import com.hyperbank.staff.employee.entity.Employee;
 import com.paulmarcelinbejan.architecture.sniper.base.BaseSniper;
-import com.paulmarcelinbejan.architecture.sniper.mapper.input.MapperInput;
-import com.paulmarcelinbejan.architecture.sniper.mapper.output.MapperOutput;
-import com.paulmarcelinbejan.architecture.sniper.service.Service;
 import com.paulmarcelinbejan.architecture.sniper.stereotype.Sniper;
-import com.paulmarcelinbejan.architecture.sniper.validator.Validator;
 
 @Sniper
 public class EmployeeSaveOneSniper extends BaseSniper<EmployeeSaveRequest, Employee, Employee, EmployeeResponse> {
 
 	public EmployeeSaveOneSniper(
-			Validator<EmployeeSaveRequest> validator,
-			MapperInput<EmployeeSaveRequest, Employee> mapperInput, 
-			Service<Employee, Employee> service,
-			MapperOutput<Employee, EmployeeResponse> mapperOutput) {
-		super(validator, mapperInput, service, mapperOutput);
+			EmployeeSaveOneRequestValidator validator,
+			EmployeeSaveOneMapper mapper, 
+			EmployeeSaveOneService service) {
+		super(validator, mapper, service, mapper);
 	}
 	
 }
