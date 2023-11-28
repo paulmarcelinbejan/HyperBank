e8b2526e709130d5b20d54294ccb5128fbf4fc19
Paul Marcelin Bejan <paulmarcelinbejan.developer@gmail.com>
2023-11-28 17:55:46 +0100

    feat: employee - saveMany

--- hyperbank-staff/src/main/java/com/hyperbank/staff/employee/api/save/many/EmployeeSaveManyMapper.java
+++ hyperbank-staff/src/main/java/com/hyperbank/staff/employee/api/save/many/EmployeeSaveManyMapper.java
@@ -2,36 +2,32 @@ package com.hyperbank.staff.employee.api.save.many;
 
 import java.util.List;
 
+import org.mapstruct.InjectionStrategy;
 import org.mapstruct.IterableMapping;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.MappingConstants;
-import org.mapstruct.Named;
 
+import com.hyperbank.staff.employee.api.find.one.EmployeeFindOneMapper;
+import com.hyperbank.staff.employee.api.save.one.EmployeeSaveOneMapper;
 import com.hyperbank.staff.employee.dto.EmployeeResponse;
 import com.hyperbank.staff.employee.dto.EmployeeSaveRequest;
 import com.hyperbank.staff.employee.entity.Employee;
+import com.hyperbank.staff.employee.mapper.EmployeeMapperConfig;
 import com.paulmarcelinbejan.architecture.sniper.mapper.input.MapperInput;
 import com.paulmarcelinbejan.architecture.sniper.mapper.output.MapperOutput;
 
-@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
-public abstract class EmployeeSaveManyMapper implements 
+@Mapper(config = EmployeeMapperConfig.class,
+		uses = { EmployeeFindOneMapper.class, EmployeeSaveOneMapper.class },
+		injectionStrategy = InjectionStrategy.CONSTRUCTOR)
+public interface EmployeeSaveManyMapper extends  
 	MapperInput<List<EmployeeSaveRequest>, List<Employee>>,
 	MapperOutput<List<Employee>, List<EmployeeResponse>> {
 	
 	@Override
-	@IterableMapping(qualifiedByName = "toSingleDomain")
-	public abstract List<Employee> toDomain(List<EmployeeSaveRequest> request);
-	
-	@Named("toSingleDomain")
-	@Mapping(target = "id", ignore = true)
-	protected abstract Employee toSingleDomain(EmployeeSaveRequest request);
+	@IterableMapping(qualifiedByName = "fromRequestToDomain")
+	List<Employee> toDomain(List<EmployeeSaveRequest> request);
 	
 	@Override
-	@IterableMapping(qualifiedByName = "toSingleResponse")
-	public abstract List<EmployeeResponse> toResponse(List<Employee> entity);
-	
-	@Named("toSingleResponse")
-	protected abstract EmployeeResponse toSingleResponse(Employee entity);
+	@IterableMapping(qualifiedByName = "fromDomainToResponse")
+	List<EmployeeResponse> toResponse(List<Employee> entity);
 	
 }
