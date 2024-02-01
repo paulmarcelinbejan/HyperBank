3d1ad2f2819d42c4240f565fe7f838df46cf7f18
Paul Marcelin Bejan <paulmarcelinbejan.developer@gmail.com>
2024-02-01 15:35:37 +0100

    fix: ManyToOne lazy

--- hyperbank-maps/src/main/java/com/hyperbank/maps/city/entity/City.java
+++ hyperbank-maps/src/main/java/com/hyperbank/maps/city/entity/City.java
@@ -4,6 +4,7 @@ import com.hyperbank.maps.country.entity.Country;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -11,6 +12,7 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
+
 import lombok.Data;
 
 @Data
@@ -24,7 +26,7 @@ public class City {
 	@Column(name = "id_city", nullable = false)
 	private Integer id;
 	
-	@ManyToOne
+	@ManyToOne(fetch = FetchType.LAZY, optional = false)
 	@JoinColumn(name = "fk_country", nullable = false)
 	private Country country;
 	
--- hyperbank-maps/src/main/java/com/hyperbank/maps/location/entity/Location.java
+++ hyperbank-maps/src/main/java/com/hyperbank/maps/location/entity/Location.java
@@ -4,6 +4,7 @@ import com.hyperbank.maps.city.entity.City;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -11,6 +12,7 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
+
 import lombok.Data;
 
 @Data
@@ -24,7 +26,7 @@ public class Location {
 	@Column(name = "id_location", nullable = false)
 	private Long id;
 	
-	@ManyToOne
+	@ManyToOne(fetch = FetchType.LAZY, optional = false)
 	@JoinColumn(name = "fk_city", nullable = false)
 	private City city;
 	
